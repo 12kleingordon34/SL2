3796d342daebeba8dfba983a39127ea35f62e846
danielt613 <dannytrent@btinternet.com>
2019-11-20 16:26:15 +0000

    added brief and basic perceptron (not kernel)

---
+++ perceptron.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 15 10:18:03 2019
+
+@author: ucabdbt
+"""
+
+import numpy as np
+
+class Perceptron(object):
+    
+    def __init__(self):
+        
+        self.w = np.array([])
+        self.num_classes = 1
+        self.M = 0
+        self.R = 0
+        
+    def train(self,X,y):
+        
+        m,d = X.shape
+        
+        self.num_classes = y.shape[1]
+        self.M = np.zeros(self.num_classes)
+        self.R = np.max(np.sum(X*X,axis=1))
+        
+        self.w = np.zeros((d,self.num_classes))
+        
+        for t in range(m):
+            
+            yhat = np.sign(np.dot(X[t,:],self.w))
+            if yhat*y[t,:] <= 0:
+                self.w += y[t]*X[t,:]
+                self.M += 1
+                
+    def predict_proba(self,X):
+        
+        return(np.dot(X,self.w))
+        
+    def predict(self,X):
+        
+        return(np.sign(self.predict_proba(self,X)))
+
+            
