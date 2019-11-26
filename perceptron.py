4b2bfbc104a7ca6780851afa4566f64e0611ff13
Dan Manela <danielmanela@gmail.com>
2019-11-26 18:59:06 +0000

    Adjusting linewidth

--- perceptron.py
+++ perceptron.py
@@ -12,14 +12,12 @@ import kernels
 class Perceptron(object):
     
     def __init__(self):
-        
         self.w = np.array([])
         self.num_classes = 1
         self.M = 0
         self.R = 0
         
     def train(self,X,y):
-        
         m,d = X.shape
         
         self.num_classes = y.shape[1]
@@ -29,18 +27,13 @@ class Perceptron(object):
         self.w = np.zeros((d,self.num_classes))
         
         for t in range(m):
-            
             yhat = np.sign(np.dot(X[t,:],self.w))
             if yhat*y[t,:] <= 0:
                 self.w += y[t]*X[t,:]
                 self.M += 1
                 
     def predict_proba(self,X):
-        
         return(np.dot(X,self.w))
         
     def predict(self,X):
-        
         return(np.sign(self.predict_proba(self,X)))
-
-            
