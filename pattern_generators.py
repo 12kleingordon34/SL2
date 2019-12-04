5805f89ae4f515670f1b295c8d34c40c7d093e48
danielt613 <dannytrent@btinternet.com>
2019-12-04 18:35:38 +0000

    least squares + jalb

---
+++ pattern_generators.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 17:45:07 2019
+
+@author: dantr
+"""
+import numpy as np
+
+def JALB(n,m,seed=None):
+    
+    if seed is not None:
+        np.random.seed(seed)
+    
+    X = -1 + 2*np.random.binomial(1,0.5,size=(m,n))
+    y = X[:,0]
+    
+    return(X,y)
+    
