12f3a933d379fea048547228b7be0da4f7cc5a79
danielt613 <dannytrent@btinternet.com>
2019-12-04 18:35:38 +0000

    least squares + jalb

---
+++ least_squares.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 17:47:14 2019
+
+@author: dantr
+"""
+import numpy as np
+
+class LinearRegression(object):
+    
+    def __init__(self):
+        
+        self.w = 0
+        
+    def train(self,X,y):
+        
+        self.w = np.linalg.solve(X.T@X,X.T@y)
+        
+    def predict(self,x):
+        
+        return(x@self.w)
+        
+    def classify(self,x):
+        
+        yhat = self.predict(x)
+        
+        return(-1 + 2*(yhat > 0))
+        
+        
