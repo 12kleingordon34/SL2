904e1c1520ea4ef10de98fc3d7c66a48b5794fc5
danielt613 <dannytrent@btinternet.com>
2019-12-15 10:46:06 +0000

    change jalb

--- pattern_generators.py
+++ pattern_generators.py
@@ -11,7 +11,7 @@ def JALB(n,m,seed=None):
     if seed is not None:
         np.random.seed(seed)
     
-    X = -1 + 2*np.random.binomial(1,0.5,size=(m,n))
+    X = -1 + 2*np.random.binomial(1,0.5,size=(m*n)).reshape(m,n)
     y = X[:,0]
     
     return(X,y)
