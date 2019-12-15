db2b015f974447c04433175a0fc5f8e9a08582f3
danielt613 <dannytrent@btinternet.com>
2019-12-15 12:31:30 +0000

    find trend

--- main_pt2.py
+++ main_pt2.py
@@ -10,12 +10,13 @@ from least_squares import LinearRegression
 from winnow import Winnow
 from perceptron import Perceptron
 from knn import OneNN
+import matplotlib.pyplot as plt
 
 def find_least_m(n, algorithm,X_test,y_test):
     
     seed = np.random.randint(10000)
     
-    m = 0
+    m = 1
     
     error = np.inf
     
@@ -43,7 +44,36 @@ def find_avg_least_m(n,algorithm,num_runs=50):
     return(np.mean(M),np.std(M))
     
 
-
+def find_trend_m(algorithm):
+    
+    M_mean = np.zeros(48)
+    M_std = np.zeros(48)
+    
+    for i in range(2,51):
+        M_mean[i],M_std[i] = find_avg_least_m(i,algorithm)
+    
+    return(M_mean,M_std)
+    
+    
+def plot_trend(M_mean,M_std):
+    
+    n = np.arange(2,M_mean.size+2)
+    plt.figure()
+    plt.errorbar(n,M_mean,yerr=M_std)
+    plt.xlabel("n")
+    plt.ylabel("m")
+    plt.show()
+    
+    
+if __name__=="__main__":
+    alg = Perceptron()
+    mean,std = find_trend_m(alg)
+    plot_trend(mean,std)
+    
+    
+    
+        
+    
     
     
         
