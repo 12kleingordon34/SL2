48e06fcfdc13eca5262906b8e6156475fff0d8ba
Dan Manela <danielmanela@gmail.com>
2019-12-16 23:32:23 +0000

    Minor cleanup

--- least_squares.py
+++ least_squares.py
@@ -7,23 +7,15 @@ Created on Wed Dec  4 17:47:14 2019
 import numpy as np
 
 class LinearRegression(object):
-    
     def __init__(self):
-        
         self.w = 0
         
     def train(self,X,y):
-        
         self.w = np.linalg.pinv(X.T@X)@X.T@y
         
     def _predict(self,x):
-        
         return(x@self.w)
         
     def predict(self,x):
-        
         yhat = self._predict(x)
-        
         return(np.sign(yhat))
-        
-        
