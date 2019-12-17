7e5af6937e211c07eb761df28e4d4d93100942de
danielt613 <dannytrent@btinternet.com>
2019-12-17 10:45:09 +0000

    winnow bug fix

--- main_pt2.py
+++ main_pt2.py
@@ -12,34 +12,34 @@ from perceptron import Perceptron
 from knn import OneNN
 import matplotlib.pyplot as plt
 
-def find_least_m(n, algorithm,X_test,y_test,start=0):
+def find_least_m(n, algorithm,X_test,y_test,start=0,neg=-1):
     seed = np.random.randint(10000)
     m = start
     error = np.inf
     while error > 0.1:
         m += 1
-        X_train, y_train = JALB(n,m,seed)
+        X_train, y_train = JALB(n,m,neg=neg,seed=seed)
         algorithm.train(X_train,y_train)
         error = np.mean(algorithm.predict(X_test)!=y_test)
     return(m)
     
 
-def find_avg_least_m(n,algorithm,start=0,num_runs=50):
+def find_avg_least_m(n,algorithm,start=0,num_runs=50,neg=-1):
     #size = int(np.log(1./0.05)/(2*(0.005)**2)) 
-    X_test,y_test = JALB(n,min(pow(2,n),60000))
+    X_test,y_test = JALB(n,6000,neg=neg)
     M = np.zeros(num_runs)
     for i in range(num_runs):
-        M[i] = find_least_m(n,algorithm,X_test,y_test,start)
+        M[i] = find_least_m(n,algorithm,X_test,y_test,start=start,neg=neg)
         start = max(0,int(M[i]-10))
     return(np.mean(M),np.std(M))
     
 
-def find_trend_m(algorithm):
+def find_trend_m(algorithm,neg=-1):
     start =0
     M_mean = np.zeros(99)
     M_std = np.zeros(99)
     for i in range(2,101):
-        M_mean[i-2],M_std[i-2] = find_avg_least_m(i,algorithm,start)
+        M_mean[i-2],M_std[i-2] = find_avg_least_m(i,algorithm,start=start,neg=neg)
         start = max(0,int(M_mean[i-2]-5))
     return(M_mean,M_std)
     
@@ -54,6 +54,7 @@ def plot_trend(M_mean,M_std):
     
     
 if __name__=="__main__":
-    alg = LinearRegression()
-    mean,std = find_trend_m(alg)
+    alg = Winnow()
+    neg = 0
+    mean,std = find_trend_m(alg,neg=neg)
     plot_trend(mean,std)
